67f9427c98083196519f3f52a778d8230dda597e
Bawaw <gaming.anon@yandex.com>
2015-03-21 19:38:43 +0100

    local login system - 2012

---
+++ UserDataBase/DataSearcher.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace UserDataBase
+{
+    class DataSearcher
+    {
+        private static string path 
+        {
+            get 
+            {
+                return "Data.txt";
+            }
+        }
+        public static bool ReadDataBase(string User, string Password)
+        {
+            bool Valid = false;
+            string line;
+            using (StreamReader Data = new StreamReader(path))
+            {
+
+                line = Data.ReadLine();
+                while (line != null)
+                {
+                    if (line.StartsWith(User))
+                    {
+                        string[] x = line.Split('|');
+                        if (x[1] == Password)
+                        {
+                            Valid = true;
+                        }
+                    }
+                    line = Data.ReadLine();
+                }
+            }
+
+            return Valid;
+        }
+
+        public static bool CheckValid(string user, string password)
+        {
+            bool Valid = true;
+            string line;
+            using (StreamReader Data = new StreamReader(path))
+            {
+                line = Data.ReadLine();
+                while (line != null)
+                {
+                    if (line.StartsWith(user))
+                    {
+                        Valid = false;
+                    }
+                    line = Data.ReadLine();
+                }
+            }
+            return Valid;
+        }
+
+        public static void CreateNewUser(string user, string password)
+        {
+            using (StreamWriter write = new StreamWriter(path, true))
+            {
+                write.WriteLine();
+                
+                write.WriteLine(user + "|" + password + "|");
+            }
+        }
+    }
+}
---
+++ UserDataBase/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UserDataBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool LogIn = false, menu = true;
+
+            string choice = "0", UserName, UserPassword, expression;
+
+            Console.WriteLine("Please Enter User Name");
+            UserName = Console.ReadLine();
+            Console.WriteLine("Please Enter Password");
+            UserPassword = Console.ReadLine();
+            LogIn = DataSearcher.ReadDataBase(UserName, UserPassword);
+
+            if (LogIn == true)
+            {
+                while (menu == true)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Welcome " + UserName + " How Can I Help You?");
+                    Console.WriteLine("Create: Create A New User");
+                    Console.WriteLine("Check: Check Number Of Users On This Network");
+                    Console.WriteLine("Edit: Acces My Profile");
+                    Console.WriteLine("EXIT");
+                    choice = Console.ReadLine();
+                    choice.ToLower();
+                    switch (choice)
+                    {
+                        case "create":
+                            Console.WriteLine("Are you sure You Want To Create A New User? yes/no");
+                            expression = Console.ReadLine();
+                            expression.ToLower();
+                            if (expression == "yes")
+                            {
+                                string user, password;
+                                Console.WriteLine("Please Enter A User Name");
+                                user = Console.ReadLine();
+                                Console.WriteLine("Please Enter A Password");
+                                password = Console.ReadLine();
+                                bool Valid = DataSearcher.CheckValid(user, password);
+                                Console.Clear();
+                                if (Valid == true)
+                                {
+                                    DataSearcher.CreateNewUser(user, password);
+                                    
+                                    Console.WriteLine("UserCreated");
+                                    Console.ReadLine();
+                                }
+                                else if (Valid == false)
+                                {
+                                    Console.WriteLine("This User Name Already Exists, Please Chose A Different One");
+                                    Console.ReadLine();
+                                }
+                            }
+                          
+                            break;
+                        case "exit":
+                            Console.WriteLine("Log Out Succesfull");
+                            menu = false;
+                            Console.ReadLine();
+                            break;
+                            default:
+                            Console.WriteLine("Invalid Action");
+                            break;
+
+                    }
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid User Name Or Password, Program terminated...");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
